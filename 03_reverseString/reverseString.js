2e87a62feae6a23cf05871df28fe7d84844a900f
Nirmal Krishnan <nirmalkrish98@gmail.com>
2023-12-26 22:48:21 +0530

    Complete 03_REVERSESTRING exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    if (text === '') {
+        return '';
+    }
 
+    const splittedString = text.split('');
+    splittedString.reverse();
+    return splittedString.join('');
 };
 
 // Do not edit below this line
