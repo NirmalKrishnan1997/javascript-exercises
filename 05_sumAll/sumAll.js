694d0fa646628e34f9095667c2f319f3004273d9
Nirmal Krishnan <nirmalkrish98@gmail.com>
2023-12-26 23:20:58 +0530

    Complete 05_SUMALL exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
 
+    if (num1 == num2) {
+        return num1;
+    }
+
+    let sum = 0;
+    let small = num1 < num2 ? num1 : num2;
+    const large = num1 > num2 ? num1 : num2;
+
+    while (small <= large) {
+        sum += small;
+        small++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
