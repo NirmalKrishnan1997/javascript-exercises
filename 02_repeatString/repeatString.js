912e76953416e28e938260635c6ce592a33cb791
Nirmal Krishnan <nirmalkrish98@gmail.com>
2023-12-26 22:37:15 +0530

    Complete 02_REPEATSTRING exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    let result = '';
+    while (num > 0) {
+        result += word;
+        num--;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
