2b9ac58be4dcc452343594ef6d8206d58864fb6f
Nirmal Krishnan <nirmalkrish98@gmail.com>
2023-12-31 19:04:33 +0530

    Complete 09_Pallindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strWithoutPunctuations = str.toUpperCase().replace(/[^\w\d]/g, "");
+    const newStr = strWithoutPunctuations.split("").reverse().join("");
 
+    return  strWithoutPunctuations === newStr;
 };
 
 // Do not edit below this line
