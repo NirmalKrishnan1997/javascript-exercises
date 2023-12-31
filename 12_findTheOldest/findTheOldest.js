93ee8547fe56ec7207f0257153b3b628dc226b0a
Nirmal Krishnan <nirmalkrish98@gmail.com>
2023-12-31 20:16:42 +0530

    Complete 12_FindTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const prevBirth = oldest.yearOfBirth;
+        const prevDeath = oldest.yearOfDeath === undefined ? (new Date()).getFullYear() : oldest.yearOfDeath;
+        const currentBirth = person.yearOfBirth;
+        const currentDeath = person.yearOfDeath === undefined ? (new Date()).getFullYear() : person.yearOfDeath;
+        const prevAge = prevDeath - prevBirth;
+        const currentAge = currentDeath - currentBirth
+        return prevAge < currentAge ? person
+         : oldest;
+    });
 };
 
 // Do not edit below this line
