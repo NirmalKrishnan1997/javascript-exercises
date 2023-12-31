7f694441a1d0f479e46ee3194114d414933fc0f1
Nirmal Krishnan <nirmalkrish98@gmail.com>
2023-12-31 19:31:25 +0530

    Complete 10_Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const number = Number.isInteger(num) ? num : Number.parseInt(num);
 
+    if (number < 0) {
+        return "OOPS"
+    }
+
+    if (number === 0) {
+        return 0;
+    }
+
+    let first = 1, second = 0;
+    for(let i = 2; i <= number; i++) {
+        let temp = first;
+        first = first + second;
+        second = temp;
+    }
+
+    return first;
 };
 
 // Do not edit below this line
